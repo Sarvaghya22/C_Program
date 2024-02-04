c5dc23dc013b7a30c6760fe698d5298ef69bb494
Sarvaghya Dewangan <138366597+Sarvaghya22@users.noreply.github.com>
2024-02-04 10:10:17 +0530

    Create bitwise.c
    You will be given an integer n, and the threshold, k. For each number i from 1 through n that are maximum value of the logical and, or and xor when compared against all the integers through n that are greater than i. consider a value only if the comparison returns a result less than k. print the results of the and ,or and exclusive or comparisons on the separate lines, in that order.
    
    Function Description
    
    Complete the calculate_the_maximum function in the editor below.
    
    calculate_the_maximum has the following parameters:
    
    int n: the highest number to consider
    int k: the result of a comparison must be lower than this number to be considered.

---
+++ bitwise.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    int count_and=0;
+    int count_or=0;
+    int count_xor=0;
+    scanf("%d %d",&n,&k);
+    for(int a=1;a<n;a++)
+    {
+        for(int b=a+1;b<=n;b++)
+        {
+            if((a&b)>count_and)
+            {
+                if((a&b)<k)
+                {
+                    count_and=a&b;
+                }
+            }
+            if((a|b)>count_or)
+            {
+                if((a|b)<k)
+                {
+                    count_or=a|b;
+                }
+            }
+            if((a^b)>count_xor)
+            {
+                if((a^b)<k)
+                {
+                    count_xor=a^b;
+                }
+            }
+        }
+    }
+    printf("%d\n%d\n%d",count_and,count_or,count_xor);
+    
+}
